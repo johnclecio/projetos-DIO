662a828d968ec0521890f8545eebdbe660a52dbe
johnclecio <johnclecio@icloud.com>
2021-12-14 10:37:22 -0300

    introdução

---
+++ javaScript/js/main.js
@@ -0,0 +1,31 @@
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva",cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push('uva');
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+
+
+
+
+
+
+//var  nome = "John Lima";
+//var n1 = 28;
+//var n2 = 10;
+//var frase = "japão é o melhor time do mundo";
+//alert( nome + " tem " + idade + " anos ");
+//alert(idade + idade2 );
+//console.log(nome);
+//console.log(n1 + n2);
+//console.log(frase.toLowerCase());
