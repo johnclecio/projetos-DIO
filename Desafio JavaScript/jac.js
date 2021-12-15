70a19d3bc3a89ecef7738ffb6db1bbc45a86a63b
johnclecio <johnclecio@icloud.com>
2021-12-15 08:17:18 -0300

    atualizado

---
+++ Desafio JavaScript/jac.js
@@ -0,0 +1,21 @@
+/*//desafio 1 
+let line = gets(11,7).split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = A + B;
+console.log("X = " + total);
+//desafio 2*/
+/*
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let total = valor1 * valor2; //troquei para a multiplicação de valo1 e valor2
+console.log("PROD = " + total);*/
+//desafio 3
+/*
+let valor1 = parseInt(gets(25));
+let valor2 = parseInt(gets(100));
+let valor3 = parseFloat(gets(5.50));
+let salary = parseFloat(valor2*valor3).toFixed(2);
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salary); */
+
